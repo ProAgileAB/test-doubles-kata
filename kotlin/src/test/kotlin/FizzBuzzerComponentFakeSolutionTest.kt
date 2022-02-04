5d6e1eaa2769197b33f7b65d4f23dd2e2903aec3
neppord <neppord@gmail.com>
2022-02-04 14:33:09 +0100

    Fix warnings

--- kotlin/src/test/kotlin/FizzBuzzerComponentFakeSolutionTest.kt
+++ kotlin/src/test/kotlin/FizzBuzzerComponentFakeSolutionTest.kt
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test
 internal class FizzBuzzerComponentFakeSolutionTest {
     @Test
     internal fun `it uses the dependency`() {
-        val sut = FizzBuzzerComponent({ when{
-            it % 15 == 0 -> "FizzBuzz"
-            it % 5 == 0 -> "Buzz"
-            it % 3 == 0 -> "Fizz"
-            else -> "${it}"
-        }})
+        val sut = FizzBuzzerComponent {
+            when {
+                it % 15 == 0 -> "FizzBuzz"
+                it % 5 == 0 -> "Buzz"
+                it % 3 == 0 -> "Fizz"
+                else -> "${it}"
+            }
+        }
         val ret = sut.print()
         assertEquals(101, ret.lines().size)
     }
