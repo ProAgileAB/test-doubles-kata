b0761cb3605c0fa11f8ba7dc6a53b3c317589645
neppord <neppord@gmail.com>
2022-02-04 14:33:57 +0100

    Fix warnings

--- kotlin/src/test/kotlin/FizzBuzzerComponentStubSolutionTest.kt
+++ kotlin/src/test/kotlin/FizzBuzzerComponentStubSolutionTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 internal class FizzBuzzerComponentStubSolutionTest {
     @Test
     internal fun `it uses the dependency`() {
-        val sut = FizzBuzzerComponent(FizzBuzzer { "test data " + it })
+        val sut = FizzBuzzerComponent { "test data $it" }
         val ret = sut.print()
         assertEquals(101, ret.lines().size)
     }
